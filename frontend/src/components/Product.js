e2808dfcd838f7275f0e2393a062b40bc5cdeff6
kcselm <kcselm93@gmail.com>
2020-11-16 19:54:59 -0600

    Fetching Products from the backend to the frontend

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text>
+        <Card.Text as='div'>
           <div className='my-3'>
             <Rating
               value={product.rating}
