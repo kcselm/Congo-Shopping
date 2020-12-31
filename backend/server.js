c24ba18f81d16b74043a38e84d70570f0f366bc4
kcselm <kcselm93@gmail.com>
2020-12-31 11:54:35 -0600

    Creating custom error handlers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-
+import { notFound, errorHandler } from './middleWare/errorMiddleWare.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -18,14 +18,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
